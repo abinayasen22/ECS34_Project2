359341c33b798e22b5c77a294ba0105960e78857
Nissi-d <johndoe@email.comnudom@ucdavis.edu>
2025-02-17 15:39:36 -0800

    deleted:    proj2/obj/StringUtils.o 	modified:   proj2/testsrc/StringUtilsTest.cpp

--- proj2/testsrc/StringUtilsTest.cpp
+++ proj2/testsrc/StringUtilsTest.cpp
@@ -2,71 +2,71 @@
 #include "StringUtils.h"
 
 TEST(StringUtilsTest, SliceTest){
-    EXPECT_EQ(Slice("Hello, World!", 0, 5), "Hello");
-    EXPECT_EQ(Slice("Hello, World!", 7), "Hello");
-    EXPECT_EQ(Slice("Hello, World!", -3), "rld!");
+    EXPECT_EQ(StringUtils::Slice("Hello, World!", 0, 5), "Hello");
+    EXPECT_EQ(StringUtils::Slice("Hello, World!", 7), "Hello");
+    EXPECT_EQ(StringUtils::Slice("Hello, World!", -3), "rld!");
 }
 
 TEST(StringUtilsTest, Capitalize){
-    EXPECT_EQ(Capitalize("hello"), "Hello");
-    EXPECT_EQ(Capitalize("WORLD"), "World");
-    EXPECT_EQ(Capitalize("3"), "3");
+    EXPECT_EQ(StringUtils::Capitalize("hello"), "Hello");
+    EXPECT_EQ(StringUtils::Capitalize("WORLD"), "World");
+    EXPECT_EQ(StringUtils::Capitalize("3"), "3");
 }
 
 TEST(StringUtilsTest, Upper){
-    EXPECT_EQ(Upper("hello"), "HELLO");
-    EXPECT_EQ(Upper("HELLO"), "HELLO");
+    EXPECT_EQ(StringUtils::Upper("hello"), "HELLO");
+    EXPECT_EQ(StringUtils::Upper("HELLO"), "HELLO");
 }
 
 TEST(StringUtilsTest, Lower){
-    EXPECT_EQ(Upper("hello"), "hello");
-    EXPECT_EQ(Upper("HELLO"), "hello");
+    EXPECT_EQ(StringUtils::Lower("hello"), "hello");
+    EXPECT_EQ(StringUtils::Lower("HELLO"), "hello");
 }
 
 TEST(StringUtilsTest, LStrip){
-    EXPECT_EQ(LStrip("   hello"), "hello");
-    EXPECT_EQ(LStrip("hello   "), "hello   ");
+    EXPECT_EQ(StringUtils::LStrip("   hello"), "hello");
+    EXPECT_EQ(StringUtils::LStrip("hello   "), "hello   ");
 }
 
 TEST(StringUtilsTest, RStrip){
-    EXPECT_EQ(RStrip("hello  "), "hello");
-    EXPECT_EQ(RStrip("   hello"), "   hello");
+    EXPECT_EQ(StringUtils::RStrip("hello  "), "hello");
+    EXPECT_EQ(StringUtils::RStrip("   hello"), "   hello");
 }
 
 TEST(StringUtilsTest, Strip){
-    EXPECT_EQ(Strip("  hello  "), "hello");
-    EXPECT_EQ(Strip("   hello   "), "hello");
+    EXPECT_EQ(StringUtils::Strip("  hello  "), "hello");
+    EXPECT_EQ(StringUtils::Strip("   hello   "), "hello");
 }
 
 TEST(StringUtilsTest, Center){
-    EXPECT_EQ(Center("hello", 8), " hello ");
-    EXPECT_EQ(Center("hello", 3), "hello");
+    EXPECT_EQ(StringUtils::Center("hello", 8), " hello ");
+    EXPECT_EQ(StringUtils::Center("hello", 3), "hello");
 }
 
 TEST(StringUtilsTest, LJust){
-    EXPECT_EQ(LJust("hello", 10), "hello     ");
+    EXPECT_EQ(StringUtils::LJust("hello", 10), "hello     ");
 }
 
 TEST(StringUtilsTest, RJust){
-    EXPECT_EQ(RJust("hello", 10), "     hello");
+    EXPECT_EQ(StringUtils::RJust("hello", 10), "     hello");
 }
 
 TEST(StringUtilsTest, Replace){
-    EXPECT_EQ(Replace("hello world", "hello", "hi"), "hi world");
+    EXPECT_EQ(StringUtils::Replace("hello world", "hello", "hi"), "hi world");
 }
 
 TEST(StringUtilsTest, Split){
-    EXPECT_EQ(Split("hello world", " "), std::vector<std::string>{"hello", "world"});
+    EXPECT_EQ(StringUtils::Split("hello world", ""), (std::vector<std::string>{"hello", "world"}));
 }
 
 TEST(StringUtilsTest, Join){
-    EXPECT_EQ(Join(",", ("a", "b", "c")), "a,b,c");
+    EXPECT_EQ(StringUtils::Join(",", {"a", "b", "c"}), "a,b,c");
 }
 
 TEST(StringUtilsTest, ExpandTabs){
-    EXPECT_EQ(ExpandTabs("hello\tworld", 4), "hello   world");
+    EXPECT_EQ(StringUtils::ExpandTabs("hello\tworld", 4), "hello   world");
 }
 
 TEST(StringUtilsTest, EditDistance){
-    EXPECT_EQ(EditDistance("hello", "hello", false), 0);
+    EXPECT_EQ(StringUtils::EditDistance("hello", "hello", false), 0);
 }
