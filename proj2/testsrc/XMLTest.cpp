f130e6eb6989c547c484b58d8441a19670e30294
abinayasen22 <asenthil@ucdavis.edu>
2025-02-17 22:58:49 -0800

    xmltest

--- proj2/testsrc/XMLTest.cpp
+++ proj2/testsrc/XMLTest.cpp
@@ -5,33 +5,25 @@
 #include "StringDataSink.h"
 
 TEST(XMLReaderTest, SimpleXML) {
-    // Test case for reading a simple XML file
+    //reading simple XML file
 }
 
 TEST(XMLReaderTest, NestedElements) {
-    // Test case for reading XML with nested elements
+    // reading XML with nested elements
 }
 
 TEST(XMLReaderTest, Attributes) {
-    // Test case for reading XML with attributes
-}
-
-TEST(XMLReaderTest, CDATA) {
-    // Test case for reading XML with CDATA
+    // reading XML with attributes
 }
 
 TEST(XMLWriterTest, SimpleXML) {
-    // Test case for writing a simple XML file
+    //  writing a simple XML file
 }
 
 TEST(XMLWriterTest, NestedElements) {
-    // Test case for writing XML with nested elements
+    //  write XML with nested elements
 }
 
 TEST(XMLWriterTest, Attributes) {
-    // Test case for writing XML with attributes
-}
-
-TEST(XMLWriterTest, CDATA) {
-    // Test case for writing XML with CDATA
+    //  write XML with attributes
 }
