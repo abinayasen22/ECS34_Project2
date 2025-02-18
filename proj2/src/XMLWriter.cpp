4ac8acd1c2c7d85026dc56afb3ed854a30089a56
abinayasen22 <asenthil@ucdavis.edu>
2025-02-17 16:15:10 -0800

    skeleton XMLR and W

---
+++ proj2/src/XMLWriter.cpp
@@ -0,0 +1,25 @@
+#include "XMLWriter.h"
+#include "DataSink.h"
+
+// Constructor for XML writer, sink specifies the data destination
+CXMLWriter::CXMLWriter(std::shared_ptr<CDataSink> sink) {
+    // Initialization code here
+}
+
+// Destructor for XML writer
+CXMLWriter::~CXMLWriter() {
+    // Cleanup code here
+}
+
+// Outputs all end elements for those that have been started
+bool CXMLWriter::Flush() {
+    // Implementation code here
+    return true;
+}
+
+// Writes out the entity to the output stream
+bool CXMLWriter::WriteEntity(const SXMLEntity &entity) {
+    // Implementation code here
+    return true;
+}
+
