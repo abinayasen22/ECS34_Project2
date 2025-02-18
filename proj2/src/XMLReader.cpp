33ae2f330ed7568e2785c5c6be8ac9ddef790432
abinayasen22 <asenthil@ucdavis.edu>
2025-02-17 22:22:09 -0800

    XML reader and writer

--- proj2/src/XMLReader.cpp
+++ proj2/src/XMLReader.cpp
@@ -6,13 +6,19 @@
 struct CXMLReader::SImplementation {
 };
 
-CXMLReader::CXMLReader(std::shared_ptr< CDataSource > src) {
+CXMLReader::CXMLReader(std::shared_ptr< CDataSource > /*src*/) {
+    // You can initialize your internal state here if needed, using 'src'
 }
+
 CXMLReader::~CXMLReader() {
 }
+
 bool CXMLReader::End() const {
-    return false;  
+    // Implement the logic to determine if the XML reading has finished
+    return false;
 }
-bool CXMLReader::ReadEntity(SXMLEntity &entity, bool skipcdata) {
-    return false;  
+
+bool CXMLReader::ReadEntity(SXMLEntity &/*entity*/, bool /*skipcdata*/) {
+    // Implement logic for reading an entity and potentially skipping cdata if needed
+    return false;
 }
--- proj2/src/XMLWriter.cpp
+++ proj2/src/XMLWriter.cpp
@@ -5,24 +5,23 @@ struct CXMLWriter::SImplementation {
 };
 
 // Constructor for XML writer, sink specifies the data destination
-CXMLWriter::CXMLWriter(std::shared_ptr<CDataSink> sink) {
-    // Initialization code here
+CXMLWriter::CXMLWriter(std::shared_ptr<CDataSink> /*sink*/) {
+    // You can initialize your internal state here if needed, using 'sink'
 }
 
 // Destructor for XML writer
 CXMLWriter::~CXMLWriter() {
-    // Cleanup code here
+    // Cleanup code here if needed
 }
 
 // Outputs all end elements for those that have been started
 bool CXMLWriter::Flush() {
-    // Implementation code here
+    // Implement the logic to flush the XML content
     return true;
 }
 
 // Writes out the entity to the output stream
-bool CXMLWriter::WriteEntity(const SXMLEntity &entity) {
-    // Implementation code here
+bool CXMLWriter::WriteEntity(const SXMLEntity &/*entity*/) {
+    // Implement the logic to write an entity to the data sink
     return true;
 }
-
